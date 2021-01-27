530c5e070618e607dca8ac2757907be0626cc33d
dominik <dominikklein96@gmx.de>
2021-01-27 13:25:10 +0100

    update login.js

--- user-client/scripts/login.js
+++ user-client/scripts/login.js
@@ -1,9 +1,16 @@
+import "../node_modules/express";
+import "../node_modules/bcrypt";
+
+
 console.log("Login process initiated.")
 
 //check login credentials
+const success = function login(username, key) {
+    console.log("Login process started with" + username + key)
+}
 
 
-if (/*login successful*/true) {
+if (success) {
     //let the user log in
     console.log("Login process successful.")
 }
