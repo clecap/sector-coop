086e13145632464eaa7bc495bf3e2eee7ce0c1dc
dominik <dominikklein96@gmx.de>
2021-02-04 14:41:21 +0100

    updated identity server

--- user-client/pages/public/scripts/register.js
+++ user-client/pages/public/scripts/register.js
@@ -1,25 +1,30 @@
 var identityServer = "http://127.0.0.1:6000/register";
 
+
 $(function () {
     $("#register-button").on('click', function(e) {
         e.preventDefault();
         
         var _username = $('#username').val();
         var _password = $('#password').val();
-        var payload = JSON.stringify({"username" : _username, "password" : _password});
+        var payload = JSON.stringify(
+            {
+                "username" : _username.toString(), 
+                "password" : _password.toString()
+            });
         
-
-        $.post(identityServer, payload, function (res) {
+        /*$.post(identityServer, payload, function (res) {
             console.log(res)
           },
         'json');
-        
-        /*$.ajax({
+        */
+        $.ajax({
             type: "POST",
             url: identityServer,
             data: payload,
-            success: success,
+            success: function(data) {alert('data: ' + data);},
+            contentType: "application/json",
             dataType: "json"
-        })*/
+        })
     })
 });
