9d76551cbde33fcef0b97c7b2d52e4be8fa06fc7
Jonas <jonaschinagorom@gmail.com>
2021-02-08 15:02:16 +0100

    some config

--- FileVsDB/back-end/pgconfig/index.js
+++ FileVsDB/back-end/pgconfig/index.js
@@ -4,7 +4,7 @@ const config = {
     database: "postgres",
     host: "localhost",
     user: "postgres",
-    password: "china@01#",
+    password: "postgres",
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000
