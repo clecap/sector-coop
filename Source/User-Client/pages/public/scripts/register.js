52bd2ea28eba549305fd464c86cdcf2fc770f840
doklein <38957086+doklein@users.noreply.github.com>
2021-02-18 16:04:48 +0100

    added second password field to register page in order to check whether the user entered the desired password on login

--- Source/User-Client/pages/public/scripts/register.js
+++ Source/User-Client/pages/public/scripts/register.js
@@ -5,30 +5,37 @@ $(function () {
         e.preventDefault();
         var username = $('#username').val();
         var password = $('#password').val();
-        var payload = JSON.stringify(
-            {
-                "username" : username.toString(), 
-                "password" : password.toString()
-            });
-        
-        $.ajax({
-            type: "POST",
-            url: identityServer,
-            data: payload,
-            success: function(data) {alert('data: ' + data);},
-            contentType: "application/json",
-            dataType: "json",
-            statusCode: {
-                201: function() {
-                    alert("Registration process was succesful!");
-                },
-                555: function() {
-                    alert("This username already exists. Please try a different one.");
-                },
-                500: function() {
-                    alert("Something went wrong!");
+        var repeated_password = $('#repeat-password').val();
+
+        if(password === repeated_password) {
+            var payload = JSON.stringify(
+                {
+                    "username" : username.toString(), 
+                    "password" : password.toString()
+                });
+            
+            $.ajax({
+                type: "POST",
+                url: identityServer,
+                data: payload,
+                success: function(data) {alert('data: ' + data);}, 
+                contentType: "application/json",
+                dataType: "json",
+                statusCode: {
+                    201: function() {
+                        alert("Registration process was succesful!");
+                    },
+                    555: function() {
+                        alert("This username already exists. Please try a different one.");
+                    },
+                    500: function() {
+                        alert("Something went wrong!");
+                    }
                 }
-            }
-        })
+            })
+        } 
+        else {
+            alert("The passwords that you entered do not match. Please try again.");
+        }
     })
 });
